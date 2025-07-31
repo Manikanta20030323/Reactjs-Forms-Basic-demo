bca1620dfc89573fd1d0d3acad8b62ca9dd55256
Manikanta20030323 <manikantadevadiga17@gmail.com>
2025-08-01 02:02:13 +0530

    addded som feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,82 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [formData, setFormData]=useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+    country:""
+  });
+console.log(formData);
+  function changeHandler(event){
+      setFormData((formData)=>({
+        ...formData,
+      [event.target.name] : event.target.value
+      }))
+  }
   return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
+   <div>
+    <form>
+      <label>First Name</label>
+      <br></br>
+      <input type="text" name="firstName" placeholder="Enter your name" onChange={changeHandler} className="border"></input>
+      <br></br>
+      <br></br>
+        <label>Last Name</label>
+      <br></br>
+      <input type="text" name="lastName" placeholder="Enter your name" onChange={changeHandler} className="border"></input>
+      <br></br>
+      <br></br>
+        <label>Email</label>
+      <br></br>
+      <input type="email" name="email" placeholder="Enter your name" onChange={changeHandler} className="border"></input>
+      <br></br>
+      <br></br>
+        <label>Country</label>
+      <br></br>
+      <select className="border" name="country" onChange={changeHandler}>
+        <option>Please select one</option>
+        <option value="India">India</option>
+        <option value="Mexico">Mexico</option>
+        <option value="USA">USA</option>
+        <option value="Canada">Canada</option>
+      </select>
+      <br></br>
+      <br></br>
+      <div>
+        <label>By Email</label>
+        <div>
+          <input type="checkbox" name="emailcheckbox" id="comment"></input>
+          <label htmlFor="comment" >Comments</label>
+        </div>
+        <div>
+          <input type="checkbox" name="emailcheckbox" id="Candidates"></input>
+          <label htmlFor="Candidates" >Candidates</label>
+        </div>
+        <div>
+          <input type="checkbox" name="emailcheckbox" id="Offers"></input>
+          <label htmlFor="Offers" >Offers</label>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          push notifications
+        </div>
+        <input type="radio" id="Everything"/>
+        <label htmlFor="Everything" >Everything</label>
+        <br/>
+        <input type="radio" id="same"/>
+        <label htmlFor="same" >same as email</label>
+        <br/>
+        <input type="radio" id="notify"/>
+        <label htmlFor="notify" >no such notify</label>
+        <br/>
+        <br/>
+      </div>
+    </form>
+   </div>
   );
 }
 
